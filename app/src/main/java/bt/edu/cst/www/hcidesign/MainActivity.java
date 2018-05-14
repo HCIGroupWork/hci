6be8596837881fcf7e6c44d1453f8e46b4559dfb
khushwanth <khushwanth.chhetri@gmail.com>
2018-05-14 21:21:13 +0600

    Upload

--- app/src/main/java/bt/edu/cst/www/hcidesign/MainActivity.java
+++ app/src/main/java/bt/edu/cst/www/hcidesign/MainActivity.java
@@ -3,7 +3,6 @@ package bt.edu.cst.www.hcidesign;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -23,8 +22,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                startActivity(new Intent(getApplication(),Contact.class));
             }
         });
     }
